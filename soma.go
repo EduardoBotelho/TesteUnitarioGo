3d1a3030380813881a27332f61ce2f25dddf078e
EduardoBotelho <edu.botelho@yahoo.com>
2025-02-20 21:46:54 -0300

    Funcao de Teste de Multiplicação Adicionada

--- soma.go
+++ soma.go
@@ -5,12 +5,21 @@ import "fmt"
 func main(){
 	x:= soma(1,2,3)
 	fmt.Println(x)
+	y:= multiplica(10,10)
+	fmt.Println(y)
 
 }
 func soma(i...int) int{
 	total :=0
 	for _,v:= range i{
 		total += v
+	}
+	return total
+}
+func multiplica(i ...int) int{
+	total := 1
+	for _, v:= range i{
+		total = total * v
 	}
 	return total
 }
--- soma_test.go
+++ soma_test.go
@@ -2,20 +2,52 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T) { //convenção de nomes ShouldSum/correct(assinatura do método)
+//Padrao Triple AAA
+// Arrange(Preparar)
+//Act (atua)
+//Assert(verificar as assercoes)
+func ShouldSumCorrect(t *testing.T) { //convenção de nomes ShouldSum/correct(assinatura do método)
+	//arrange
 	teste := soma(3, 2, 1)
+	//act
 	resultado := 6
 
+	//assert
 	if teste != resultado {
 		t.Error("Valor Esperado: ", resultado, "Valor retornado: ", teste)
 	}
 }
-func TestSoma2(t *testing.T) { //ShouldSumIncorrect
+func ShouldSumIncorrect(t *testing.T) { //ShouldSumIncorrect
+	//arrange
 	teste := soma(3, 2, 1)
+	//act
 	resultado := 7
 
+	//assert
 	if teste != resultado {
 		t.Error("Valor Esperado: ", resultado, "Valor retornado: ", teste)
+	}
+
+}
+func ShouldMultCorrect(t *testing.T) {
+	//arrange
+	teste := multiplica(10, 10)
+	//act
+	resultado := 100
+	//assert
+	if teste != resultado {
+		t.Error("Valor Esperado:", resultado, "Valor retornado:", teste)
+	}
+
+}
+func ShouldMultInCorrect(t *testing.T) {
+	//arrange
+	teste := multiplica(10, 10)
+	//act
+	resultado := 2560
+	//assert
+	if teste != resultado {
+		t.Error("Valor Esperado:", resultado, "Valor retornado:", teste)
 	}
 
 }
